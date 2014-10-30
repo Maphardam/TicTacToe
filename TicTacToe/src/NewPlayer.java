df3f0c4580fe4a449ebb71e27d824d84d0efea96
Tim Sabsch <t.sabsch@arcor.de>
2014-10-30 20:56:44 +0100

    some bugfixes, some restructuring. TODO: fix this new ugly check3D structure. I hate it...

--- TicTacToe/src/NewPlayer.java
+++ TicTacToe/src/NewPlayer.java
@@ -1,7 +1,11 @@
 import de.ovgu.dke.teaching.ml.tictactoe.api.IBoard;
+import de.ovgu.dke.teaching.ml.tictactoe.api.IMove;
 import de.ovgu.dke.teaching.ml.tictactoe.api.IPlayer;
 import de.ovgu.dke.teaching.ml.tictactoe.api.IllegalMoveException;
 import de.ovgu.dke.teaching.ml.tictactoe.game.Move;
+
+import java.io.ObjectInputStream.GetField;
+import java.util.Arrays;
 
 /**
  * Some comments ...
@@ -10,7 +14,7 @@ import de.ovgu.dke.teaching.ml.tictactoe.game.Move;
  */
 public class NewPlayer implements IPlayer {
 
-	private double[] weights = new double[10];
+	private double[] weights = {0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1};
 	private boolean learning = true;
 
 	public String getName() {
@@ -23,9 +27,9 @@ public class NewPlayer implements IPlayer {
 
 		// create a clone of the board that can be modified
 		IBoard copy = board.clone();
-		System.out.println(board.getSize());
-		int[] movePos = decideBestMove(copy);
 
+		int[] movePos = decideBestMove(copy);
+		System.out.println(Arrays.toString(movePos));
 		// do a move using the cloned board
 		try {
 			copy.makeMove(new Move(this, movePos));
@@ -51,7 +55,7 @@ public class NewPlayer implements IPlayer {
 	// //////////////////////////////////////////////////////////////////////////////////////////
 
 	private int[] decideBestMove(IBoard board) {
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 		double maxValue = -100;
 		int[] bestMove = null;
 
@@ -59,19 +63,15 @@ public class NewPlayer implements IPlayer {
 			for (int y = 0; y < dim; y++) {
 				for (int z = 0; z < dim; z++) {
 
-					if (board.getFieldValue(new int[] { x, y, z }) != null) {
-						try {
-							board.makeMove(new Move(this, new int[] { x, y, z }));
-						} catch (IllegalMoveException e) {
-							break;
-						}
-						double value = computeTargetFunction(board);
-						if (value > maxValue) {
-							maxValue = value;
-							bestMove = new int[] { x, y, z };
-						}
+					if (board.getFieldValue(new int[] { x, y, z }) != null)
+						break;
 
+					double value = computeValueAtPosition(board, x, y, z);
+					if (value > maxValue) {
+						maxValue = value;
+						bestMove = new int[] { x, y, z };
 					}
+
 				}
 			}
 		}
@@ -79,15 +79,31 @@ public class NewPlayer implements IPlayer {
 		return bestMove;
 	}
 
-	private double computeTargetFunction(IBoard board) {
-		int[] features = getFeatures(board);
 
-		double sum = 0;
+	private double computeValueAtPosition (IBoard _board, int x, int y, int z) {
+		
+		IBoard board = _board.clone();
+		
+		int[] currentFeatures = getFeatures(board);
+		
+		double currentSum = 0;
 		for (int i = 0; i < 10; i++) {
-			sum += features[i] * weights[i];
+			currentSum += currentFeatures[i] * weights[i];
 		}
 
-		return sum;
+		try {
+			board.makeMove(new Move(this, new int[] {x,y,z}));
+		} catch(IllegalMoveException e) {}
+		
+		int[] futureFeatures = getFeatures(board);
+		
+		double futureSum = 0;
+		for (int i = 0; i < 10; i++) {
+			futureSum += futureFeatures[i] * weights[i];
+		}
+		
+		if (x == 2 && y == 2 && z == 2) System.out.println();
+		return futureSum - currentSum;		
 	}
 
 	private int[] getFeatures(IBoard board) {
@@ -105,13 +121,12 @@ public class NewPlayer implements IPlayer {
 			features[i] += xLines[i] + yLines[i] + zLines[i] + diagXLines[i]
 					+ diagYLines[i] + diagZLines[i] + diag3DLines[i];
 		}
-		System.out.println(java.util.Arrays.toString(features));
 		return features;
 	}
 
 	private int[] checkX(IBoard board) {
 		int[] result = new int[10];
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 		// Traverse board in horizontal x direction
 		for (int z = 0; z < dim; z++) {
 			for (int y = 0; y < dim; y++) {
@@ -156,7 +171,7 @@ public class NewPlayer implements IPlayer {
 
 	private int[] checkY(IBoard board) {
 		int[] result = new int[10];
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 
 		// Traverse board in horizontal y direction
 		for (int x = 0; x < dim; x++) {
@@ -202,7 +217,7 @@ public class NewPlayer implements IPlayer {
 
 	private int[] checkZ(IBoard board) {
 		int[] result = new int[10];
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 
 		// Traverse board in horizontal z direction
 		for (int x = 0; x < dim; x++) {
@@ -248,7 +263,7 @@ public class NewPlayer implements IPlayer {
 
 	private int[] checkDiagX(IBoard board) {
 		int[] result = new int[10];
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 
 		// Traverse board in diagonal z direction
 		for (int y = 0; y < dim; y++) {
@@ -291,7 +306,7 @@ public class NewPlayer implements IPlayer {
 
 	private int[] checkDiagY(IBoard board) {
 		int[] result = new int[10];
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 
 		// Traverse board in diagonal z direction
 		for (int z = 0; z < dim; z++) {
@@ -334,7 +349,7 @@ public class NewPlayer implements IPlayer {
 
 	private int[] checkDiagZ(IBoard board) {
 		int[] result = new int[10];
-		int dim = board.getDimensions();
+		int dim = board.getSize();
 
 		// Traverse board in diagonal z direction
 		for (int x = 0; x < dim; x++) {
@@ -374,18 +389,30 @@ public class NewPlayer implements IPlayer {
 		}
 		return result;
 	}
-
+	
 	private int[] check3D(IBoard board) {
+		int[] result0 = check3D_0(board);
+		int[] result1 = check3D_1(board);
+		int[] result2 = check3D_2(board);
+		int[] result3 = check3D_3(board);
+		
+		int[] result = new int[10];
+		
+		for (int i = 0; i < 10; i++)
+			result[i] = result0[i] + result1[i] + result2[i] + result3[i];
+		
+		return result;
+	}
+
+	private int[] check3D_0(IBoard board) {
 		IPlayer player = null;
 		int count = 0;
+		int[] result = new int[10];
 		
 		for(int i = 0; i < 5; i++){
-			// Get player occupying field
+
 			IPlayer occupier = board.getFieldValue(new int[] { i, i, i });
-			// When it is the first field the row the player to count is
-			// set.
-			// It does not matter which player is counted, as the row is not
-			// valid i two players occupy fields there.
+
 			if (occupier != null) {
 				if (player == null) {
 					player = occupier;
@@ -400,7 +427,106 @@ public class NewPlayer implements IPlayer {
 				}
 			}
 		}
-		return null;
+		if (count != 0) {
+			if (player == this)
+				result[count - 1]++;
+			else
+				result[count + 4]++;
+		}
+		return result;
+	}
+	private int[] check3D_1(IBoard board) {
+		IPlayer player = null;
+		int count = 0;
+		int[] result = new int[10];
+		
+		for(int i = 0; i < 5; i++){
+
+			IPlayer occupier = board.getFieldValue(new int[] { 4-i, i, i });
+
+			if (occupier != null) {
+				if (player == null) {
+					player = occupier;
+					count++;
+				} else {
+					if (player != occupier) {
+						count = 0;
+						break;
+					} else {
+						count++;
+					}
+				}
+			}
+		}
+		if (count != 0) {
+			if (player == this)
+				result[count - 1]++;
+			else
+				result[count + 4]++;
+		}
+		return result;
+	}
+	private int[] check3D_2(IBoard board) {
+		IPlayer player = null;
+		int count = 0;
+		int[] result = new int[10];
+		
+		for(int i = 0; i < 5; i++){
+
+			IPlayer occupier = board.getFieldValue(new int[] { i, 4-i, i });
+
+			if (occupier != null) {
+				if (player == null) {
+					player = occupier;
+					count++;
+				} else {
+					if (player != occupier) {
+						count = 0;
+						break;
+					} else {
+						count++;
+					}
+				}
+			}
+		}
+		if (count != 0) {
+			if (player == this)
+				result[count - 1]++;
+			else
+				result[count + 4]++;
+		}
+		return result;
+	}
+	private int[] check3D_3(IBoard board) {
+		IPlayer player = null;
+		int count = 0;
+		int[] result = new int[10];
+		
+		for(int i = 0; i < 5; i++){
+
+			IPlayer occupier = board.getFieldValue(new int[] { i, i, 4-i });
+
+			if (occupier != null) {
+				if (player == null) {
+					player = occupier;
+					count++;
+				} else {
+					if (player != occupier) {
+						count = 0;
+						break;
+					} else {
+						count++;
+					}
+				}
+			}
+		}
+		if (count != 0) {
+			if (player == this)
+				result[count - 1]++;
+			else
+				result[count + 4]++;
+		}
+		return result;
 	}
 
 	private boolean initWeights() {
